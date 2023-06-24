e4f232d021aa8efe25bbfb2578e3ed1ee8c85b19
Arch0125 <dasarchisman25@gmail.com>
2023-06-24 10:28:58 +0530

    add ethers

--- src/initaa.ts
+++ src/initaa.ts
@@ -1,23 +1,21 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { task } from "hardhat/config";
-import "@nomiclabs/hardhat-ethers";
+import { ethers } from "ethers";
 import { SimpleAccountAPI } from "@account-abstraction/sdk";
 
 const InitAA = async (hre: HardhatRuntimeEnvironment) => {
-  const { ethers } = hre;
-
   const mnemonic = "test test test test test test test test test test test junk";
 
   const entryPointAddress = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
   const factoryAddress = "0x9406Cc6185a346906296840746125a0E44976454";
   const rpcUrl = "http://localhost:8545";
 
-  const provider = new hre.ethers.providers.JsonRpcProvider(rpcUrl);
+  const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
 
   const wallets = [];
   var scw = [];
   for (let i = 0; i < 10; i++) {
-    const wallet = hre.ethers.Wallet.fromMnemonic(mnemonic, `m/44'/60'/0'/0/${i}`);
+    const wallet = ethers.Wallet.fromMnemonic(mnemonic, `m/44'/60'/0'/0/${i}`);
     wallets.push(wallet);
   }
 
