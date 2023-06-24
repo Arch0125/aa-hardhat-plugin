836b5775b52af0748d1a2e14ff45af205cb629c2
Arch0125 <dasarchisman25@gmail.com>
2023-06-24 10:21:15 +0530

    fix:init aa

--- src/initaa.ts
+++ src/initaa.ts
@@ -5,20 +5,19 @@ import { SimpleAccountAPI } from "@account-abstraction/sdk";
 
 const InitAA = async (hre: HardhatRuntimeEnvironment) => {
   const { ethers } = hre;
-  const mnemonic =
-    "either clever polar crack agree edge area effort portion movie outdoor drastic";
+
+  const mnemonic = "test test test test test test test test test test test junk";
 
   const entryPointAddress = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
   const factoryAddress = "0x9406Cc6185a346906296840746125a0E44976454";
-  const rpcUrl =
-    "https://api.stackup.sh/v1/node/09b0b15c992db3bc056c367840e10df3e816ea4a80a6a144d06d4e3f6a6547d1";
+  const rpcUrl = "http://localhost:8545";
 
-  const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+  const provider = new hre.ethers.providers.JsonRpcProvider(rpcUrl);
 
   const wallets = [];
   var scw = [];
   for (let i = 0; i < 10; i++) {
-    const wallet = ethers.Wallet.fromMnemonic(mnemonic, `m/44'/0'/0'/0/${i}`);
+    const wallet = hre.ethers.Wallet.fromMnemonic(mnemonic, `m/44'/60'/0'/0/${i}`);
     wallets.push(wallet);
   }
 
